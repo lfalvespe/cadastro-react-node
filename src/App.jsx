6e1f8c98b38b9ad48437bc56b526be5a90ef11ad
Fernando Alves <lfalvespe@gmail.com>
2025-09-01 23:21:52 -0300

    react project created with vite

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <h1>Cadastro React + Node JS</h1>
+      <div className="card">
+      
+      </div>
+      <p className="read-the-docs">
+        Under development
+      </p>
+    </>
+  )
+}
+
+export default App
